79c6dbf27fb743993bb9c882f2b49472adedb8f3
Yash03joshi <joskeshu@gmail.com>
2024-07-10 18:24:59 +0530

    nav-bar

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-var a=document.getElementById("btn");
-var b=0;
-function shift() {
-    if (b==0) {
-        document.getElementsByClassName("header__large").animation="right 1.5s forwards";
-        console.log("0");
-        b=1;
-    }
-    // if (b==1) {
-    //     document.getElementsByClassName("header__large").animation="left 1.5s forwards";
-    //     console.log("1");
-    //     b=0;
-    // }
-}
-document.addEventListener("click",()=>{
-    shift();
-})
+
+var a=0;
+function myFunction() {
+    var element = document.getElementById("headerSmall");
+    document.getElementById("large").classList.toggle("mystyle");
+  }
